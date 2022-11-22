82ee04ace3c79e25ce9237c3828d715b9d7a585d
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-22 15:35:17 +0100

    dati i tipi per ogni variabile

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var TitoloLibro = "Clean Code";
+var AutoreLibro = "Robert C. Martin";
+long Isbn = 9780132350884;
+short NumeroPagine = 431;
+float Larghezza = 17.78f;
+float Altezza = 23.37f;
+float Profondità = 2.79f;
+var Dimensioni = Larghezza + " x" + Altezza + " x" + Profondità + " cm";
+float MediaValutazione = 4.7f;
+int NumeroRecensioni = 4572;
+bool Kindle = true;
+bool CopertinaFlessibile = false;
+
+
+
+
+
+
